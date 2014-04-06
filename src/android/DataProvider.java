e6a248a84f40542c60b9dcc453df4fdc35c9a4ab
dborysevich <dm.borisevich@gmail.com>
2014-04-06 12:40:15 +0300

    updated java class

--- src/android/DataProvider.java
+++ src/android/DataProvider.java
@@ -1,24 +1,4 @@
-/**
- * PKVideoThumbnail
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy of this
- * software and associated documentation files (the "Software"), to deal in the Software
- * without restriction, including without limitation the rights to use, copy, modify,
- * merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
- * permit persons to whom the Software is furnished to do so, subject to the following
- * conditions:
- * 
- * The above copyright notice and this permission notice shall be included in all copies
- * or substantial portions of the Software.
- * 
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
- * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
- * PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
- * LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT
- * OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
- * OTHER DEALINGS IN THE SOFTWARE.
- */
-package com.photokandy.PKVideoThumbnail;
+package pan.enrollmentforecast.DataProvider;
 
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CallbackContext;
@@ -26,17 +6,12 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import android.graphics.Bitmap;
-import android.graphics.Bitmap.CompressFormat;
-import android.media.*;
-import android.provider.MediaStore;
-
 import java.io.*;
 
 /**
  * This class echoes a string called from JavaScript.
  */
-public class PKVideoThumbnail extends CordovaPlugin {
+public class DataProvider extends CordovaPlugin {
 
     /**
      * Executes the request and returns PluginResult.
@@ -48,45 +23,9 @@ public class PKVideoThumbnail extends CordovaPlugin {
      */
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
         try {
-            if (action.equals("createThumbnail")) {
-                String sourceVideo = args.getString(0);
-                String targetImage = args.getString(1);
-                
-                Bitmap thumbnail = ThumbnailUtils.createVideoThumbnail ( sourceVideo.substring(7), MediaStore.Images.Thumbnails.MINI_KIND);
-                
-                FileOutputStream theOutputStream;
-                try
-                {
-                	File theOutputFile = new File (targetImage.substring(7));
-                	if (!theOutputFile.exists())
-                	{
-                		if (!theOutputFile.createNewFile())
-                		{
-                                        callbackContext.error ( "Could not save thumbnail." );
-                                        return true;
-                		}
-                	}
-                	if (theOutputFile.canWrite())
-                	{
-                		theOutputStream = new FileOutputStream (theOutputFile);
-                		if (theOutputStream != null)
-                		{
-                			thumbnail.compress(CompressFormat.JPEG, 75, theOutputStream);
-                		}
-                		else
-                		{
-                                        callbackContext.error ( "Could not save thumbnail; target not writeable");
-                                        return true;
-                		}
-                	}
-                }
-                catch (IOException e)
-                {
-                	e.printStackTrace();
-                        callbackContext.error ( "I/O exception saving thumbnail" );
-                        return true; 
-                }
-                callbackContext.success ( targetImage );        
+            System.out.println("In execute! ")
+            if (action.equals("getExamList")) {
+                callbackContext.success ( "Success!!!" );        
                 return true; 
                 
             } else {
